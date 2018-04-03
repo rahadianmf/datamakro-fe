af208685ac1cc0fcee2d5b66f0ec049c77ce624c
rahadianmf <rahadianmf@hotmail.com>
2018-04-04 02:46:59 +0700

    page search done , add page pdb.php

--- search-result.php
+++ search-result.php
@@ -3,47 +3,47 @@
 	<div class="wrapper">
 		<aside>
 			<form>
-				<b class = "blue">Frequencies</b>
-				<div class="side-filter">
+				<b class = "c-blue">Frequencies</b>
+				<label class="side-filter">Quarterly
+					<input type = "checkbox" checked="checked" name = "sf"/>
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">Annual
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">Quarterly</span>
-				</div>	
-				<div class="side-filter">
-					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">Annual</span>
-				</div>
+				  	<span class="checkmark"></span>
+				</label>
 				<br/>
-				<b class = "blue">Sources</b>
-				<div class="side-filter">
+				<b class = "c-blue">Sources</b>
+				<label class="side-filter">Badan Pusat Statistik
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">Badan Pusat Statistik</span>
-				</div>	
-				<div class="side-filter">
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">World Bank
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">World Bank</span>
-				</div>
+				  	<span class="checkmark"></span>
+				</label>
 				<br/>
-				<b class = "blue">Geographies</b>
-				<div class="side-filter">
+				<b class = "c-blue">Geographies</b>
+				<label class="side-filter">Indonesia
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">Indonesia</span>
-				</div>	
-				<div class="side-filter">
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">NAD
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">NAD</span>
-				</div>
-				<div class="side-filter">
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">North Sumatera
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">North Sumatera</span>
-				</div>	
-				<div class="side-filter">
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">West Sumatera
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">West Sumatera</span>
-				</div>
-				<div class="side-filter">
+				  	<span class="checkmark"></span>
+				</label>
+				<label class="side-filter">South Sumatera
 					<input type = "checkbox" name = "sf"/>
-					<span class = "sf-title">South Sumatera</span>
-				</div>
+				  	<span class="checkmark"></span>
+				</label>
 				<br/>
 				<a class="b-link" href="">Release</a>			
 				<a class="b-link" href="">Seasonal Year</a>			
@@ -51,9 +51,11 @@
 			</form>
 		</aside>
 		<article class = "two-column">
-			<h4>9 Search Resuls for <i>"DataMakro"</i></h4>
-			<div class="box hbb b-gray">
-				<b class = "blue">Produk Domestik Bruto Atas Harga Konstan</b>
+			<h2 class="ch-title hm">9 Search Resuls for <i>"DataMakro"</i></h2>
+			<div class="box bg-gray5">
+				<b class = "c-blue ch-title">Produk Domestik Bruto Atas Harga Konstan</b>
+			</div>
+			<div class="box hbb bg-gray5">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
@@ -61,7 +63,7 @@
 					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
 				</div>
 			</div>
-			<div class="box b-gray">
+			<div class="box hbb bg-gray5">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
@@ -69,7 +71,7 @@
 					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
 				</div>
 			</div>
-			<div class="box b-gray">
+			<div class="box bg-gray5">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
@@ -79,12 +81,22 @@
 			</div>
 			<br/>
 			<br/>
-			<div class="box hbb b-gray">
-				<b class = "blue">Produk Domestik Bruto Atas Harga Konstan</b>
+			<div class="box">
+				<b class = "c-blue ch-title">Produk Domestik Bruto Atas Harga Konstan</b>
+			</div>
+			<div class="box hbb">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
 					<span class = "i">06 November 2017</span>
+					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
+				</div>
+			</div>
+			<div class="box hbb">
+				<div class = "search-list">
+					<b>Tahun dasar 2010</b>
+					<p>Q1 2010 - Q3 2017</p>
+					<span class = "i">discontinue</span>
 					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
 				</div>
 			</div>
@@ -96,18 +108,12 @@
 					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
 				</div>
 			</div>
+			<br/>
+			<br/>
 			<div class="box">
-				<div class = "search-list">
-					<b>Tahun dasar 2010</b>
-					<p>Q1 2010 - Q3 2017</p>
-					<span class = "i">discontinue</span>
-					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
-				</div>
+				<b class = "c-blue ch-title">Produk Domestik Bruto Atas Harga Konstan</b>
 			</div>
-			<br/>
-			<br/>
-			<div class="box hbb b-gray">
-				<b class = "blue">Produk Domestik Bruto Atas Harga Konstan</b>
+			<div class="box hbb">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
@@ -115,7 +121,7 @@
 					<a class = "btn bbor" href=""><span class = "splus"></span> Add Watchlist</a>
 				</div>
 			</div>
-			<div class="box">
+			<div class="box hbb">
 				<div class = "search-list">
 					<b>Tahun dasar 2010</b>
 					<p>Q1 2010 - Q3 2017</p>
